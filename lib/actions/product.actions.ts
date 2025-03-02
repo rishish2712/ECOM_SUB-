e1fe58bba81e2117e268b861d1d3ddd2d6e0524a
MakPr016 <makpr0168@gmail.com>
2025-03-02 16:36:29 +0530

    Some update

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,40 +1,60 @@
 import { connectToDatabase } from "../db";
 import Product, { IProduct } from '@/lib/db/models/product.model'
+import { ProductInputSchema } from '../validator'
+import { IProductInput } from '@/types'
 
 export async function getAllCategories() {
     await connectToDatabase();
-    const categories = await Product.find({isPublished : true}).distinct('category');
+    const categories = await Product.find({ isPublished: true }).distinct('category');
     return categories;
 }
 
 export async function getProductsForCard({
-  tag,
-  limit = 4,
+    tag,
+    limit = 4,
 }: {
-  tag: string
-  limit?: number
+    tag: string
+    limit?: number
 }) {
-  await connectToDatabase()
-  const products = await Product.find(
-    { tags: { $in: [tag] }, isPublished: true },
-    {
-      name: 1,
-      href: { $concat: ['/product/', '$slug'] },
-      image: { $arrayElemAt: ['$images', 0] },
-    }
-  )
-    .sort({ createdAt: 'desc' })
-    .limit(limit)
-  return JSON.parse(JSON.stringify(products)) as {
-    name: string
-    href: string
-    image: string
-  }[]
+    await connectToDatabase()
+    const products = await Product.find(
+        { tags: { $in: [tag] }, isPublished: true },
+        {
+            name: 1,
+            href: { $concat: ['/product/', '$slug'] },
+            image: { $arrayElemAt: ['$images', 0] },
+        }
+    )
+        .sort({ createdAt: 'desc' })
+        .limit(limit)
+    return JSON.parse(JSON.stringify(products)) as {
+        name: string
+        href: string
+        image: string
+    }[]
 }
 
-export async function getProductBySlug(slug : string) {
+// GET PRODUCTS BY TAG
+export async function getProductsByTag({
+    tag,
+    limit = 10,
+}: {
+    tag: string
+    limit?: number
+}) {
+    await connectToDatabase()
+    const products = await Product.find({
+        tags: { $in: [tag] },
+        isPublished: true,
+    })
+        .sort({ createdAt: 'desc' })
+        .limit(limit)
+    return JSON.parse(JSON.stringify(products)) as IProduct[]
+}
+
+export async function getProductBySlug(slug: string) {
     await connectToDatabase();
-    const product = await Product.findOne({slug, isPublished : true})
-    if(!product) throw new Error('Product not found');
+    const product = await Product.findOne({ slug, isPublished: true })
+    if (!product) throw new Error('Product not found');
     return JSON.parse(JSON.stringify(product)) as IProduct;
 }
