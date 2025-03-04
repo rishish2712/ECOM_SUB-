cb7dc83bf864cc4b1b23e5230df09583b0db3948
Rishish <rishish2712@gmail.com>
2025-03-04 22:36:20 +0530

    Update checkout-form.tsx

--- app/checkout/checkout-form.tsx
+++ app/checkout/checkout-form.tsx
@@ -47,16 +47,8 @@ import { createOrder } from '@/lib/actions/order.actions'
 
 const shippingAddressDefaultValues =
   process.env.NODE_ENV === 'development'
-    ? {
-        fullName: 'Basir',
-        street: '1911, 65 Sherbrooke Est',
-        city: 'Montreal',
-        province: 'Quebec',
-        phone: '4181234567',
-        postalCode: 'H2X 1C4',
-        country: 'Canada',
-      }
-    : {
+    ? 
+  {
         fullName: '',
         street: '',
         city: '',
@@ -65,6 +57,7 @@ const shippingAddressDefaultValues =
         postalCode: '',
         country: '',
       }
+  : ""
 
 const CheckoutForm = () => {
   const router = useRouter()
@@ -92,7 +85,6 @@ const CheckoutForm = () => {
 
   const shippingAddressForm = useForm<ShippingAddress>({
     resolver: zodResolver(ShippingAddressSchema),
-    defaultValues: shippingAddress || shippingAddressDefaultValues,
   })
   const onSubmitShippingAddress: SubmitHandler<ShippingAddress> = (values) => {
     setShippingAddress(values)
