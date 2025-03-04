00384b9af90d17948ae4e47125be33b915560c72
Rishish <rishish2712@gmail.com>
2025-03-04 22:34:57 +0530

    Update menu.tsx

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -8,13 +8,10 @@ export default function Menu() {
   return (
     <div className='flex justify-end'>
       <nav className='flex gap-3 w-full'>
-        <Link href='/cart' className='header-button'>
+        <Link href='/sign-in' className='header-button'>
           <UserIcon className='h-8 w-8' />
           <span className='font-bold'>Sign in</span>
         </Link>
-        <Link href='/sign-in' className='flex items-center header-button'>
--          Hello, Sign in
--        </Link>
         <UserButton />
 
         <CartButton />
