707299f143977f82a5dd69ca56c94ab12cc3f358
rishish2712 <rishish2712@gmail.com>
2025-06-30 20:19:58 +0530

    change in footer

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,4 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import { handlers } from '@/auth'
 
-export default NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-    ],
-    secret: process.env.NEXTAUTH_SECRET,
-});
+export const { GET, POST } = handlers
+
